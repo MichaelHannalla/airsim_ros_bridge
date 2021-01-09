56a74467bd28cc09ab6917eb8b29acf9a6487bff
Moataz Elashery <moatazelashery@gmail.com>
2021-01-09 20:47:55 +0200

    Adapted Vehicle mover

--- src/airsim_ros_bridge/vehicle_mover.py
+++ src/airsim_ros_bridge/vehicle_mover.py
@@ -3,6 +3,10 @@
 import airsim 
 import rospy
 from std_msgs.msg import Float32
+from std_msgs.msg import Float64MultiArray
+from std_msgs.msg import MultiArrayLayout
+from std_msgs.msg import MultiArrayDimension
+
 
 # Initializing airsim client node and enabling API control
 client = airsim.CarClient()
@@ -15,10 +19,11 @@ car_controls.is_manual_gear = False
 rospy.init_node("airsim_vehicle_mover")
 r = rospy.Rate(2.0)
 
+
 # Defining subscriber callback functions
 def throttle_callback(throttle_input):
 	try:
-		car_controls.throttle = float(throttle_input.data)
+		car_controls.throttle = float(throttle_input.data[0])
 		client.setCarControls(car_controls)
 		r.sleep()
 	except (RuntimeError, ValueError, BufferError, IOError, AssertionError, TypeError, IndexError):
@@ -28,15 +33,15 @@ def throttle_callback(throttle_input):
 def steering_angle_callback(steering_angle_input):
 	#client=airsim.CarClient()
 	try:
-		car_controls.steering = float(steering_angle_input.data)
+		car_controls.steering =  float(steering_angle_input.data[0])
 		client.setCarControls(car_controls)
 		r.sleep()
 	except (RuntimeError, ValueError, BufferError, IOError, AssertionError, TypeError, IndexError):
 		pass
 		#print("Catching exceptions")
 
-rospy.Subscriber("SteeringAngleData", Float32, queue_size= 1, callback= steering_angle_callback)
-rospy.Subscriber("SpeedData", Float32, queue_size= 1, callback= throttle_callback)
+rospy.Subscriber('controls/AI2VCU_Steer', Float64MultiArray, queue_size= 1, callback= steering_angle_callback)
+rospy.Subscriber('controls/AI2VCU_Drive_F', Float64MultiArray, queue_size= 1, callback= throttle_callback)
 
 while not rospy.is_shutdown():
 	try:
