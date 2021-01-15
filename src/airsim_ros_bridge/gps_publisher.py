1cd8de0ec3f0533149ebbfd0923d953da36ec1ce
Ahmad Samy <imahmadsamy@outlook.com>
2021-01-15 22:11:10 +0200

    Publishing GPS velocity feature added.

--- src/airsim_ros_bridge/gps_publisher.py
+++ src/airsim_ros_bridge/gps_publisher.py
@@ -5,23 +5,37 @@ import airsim
 import rospy
 
 from sensor_msgs.msg import NavSatFix
+from geometry_msgs.msg import Vector3Stamped
 
 client = airsim.CarClient()
 client.confirmConnection()
 
 ros_gps = NavSatFix()
+ros_gps_vel = Vector3Stamped()
+
 ros_gps.status.service = ros_gps.status.SERVICE_GPS
-ros_gps.header.frame_id = "odom" 
+ros_gps.header.frame_id = "odom"
+ros_gps_vel.header.frame_id = "gps"
 
 rospy.init_node("airsim_gps_node")
-pub = rospy.Publisher('airsim/gps/fix', NavSatFix, queue_size=10)
+pos_pub = rospy.Publisher('airsim/gps/fix', NavSatFix, queue_size=10)
+vel_pub = rospy.Publisher('airsim/gps/velocity', Vector3Stamped, queue_size=10)
 
 while not rospy.is_shutdown():
 	airsim_gps = client.getGpsData(gps_name = "Gps", vehicle_name = "")
+
 	ros_gps.altitude = airsim_gps.gnss.geo_point.altitude
 	ros_gps.longitude = airsim_gps.gnss.geo_point.longitude 
 	ros_gps.latitude = airsim_gps.gnss.geo_point.latitude
-	ros_gps.header.stamp = rospy.Time.now()
-	pub.publish(ros_gps)
+	
+	ros_gps.header.stamp = rospy.Time.from_sec(airsim_gps.time_stamp / 1e9)
+	ros_gps_vel.header.stamp = rospy.Time.from_sec(airsim_gps.time_stamp / 1e9)
+	
+	ros_gps_vel.vector.x = airsim_gps.gnss.velocity.x_val
+	ros_gps_vel.vector.y = airsim_gps.gnss.velocity.y_val
+	ros_gps_vel.vector.z = airsim_gps.gnss.velocity.z_val
+
+	pos_pub.publish(ros_gps)
+	vel_pub.publish(ros_gps_vel)
 
 
